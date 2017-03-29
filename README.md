10f0362f6a0b72a4e06cb7d3ec76626778fc8e5b
Benjamin O'Keefe <benjamin.okeefe@inrix.com>
2017-03-28 17:09:00 -0700

    Wired buttons to click handlers

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gm-dialogs-poc
 This app is intended to serve as a proof of concept for using existing OpenCar controls to implement a carousel and a context menu.
+
+**This code is meant to produce a POC, and should not be used as an example of best practices for app implementation.**
--- appView.js
+++ appView.js
@@ -3,26 +3,34 @@ define(function (require) {
 
     const
         ModuleView = require('common/platform/ModuleView'),
-        Button = require('common/ui/Button');
+        Button = require('common/ui/Button'),
+        DialogConfig = require('common/ui/dialogs/DialogConfig'),
+        Dialog = require('common/ui/Dialog');
 
     return class extends ModuleView {
         started () {
             return super.started().then(() => {
-                let carouselButton, contextMenuButton; 
+                let carouselButton, contextMenuButton, carouselDialogConfig, contextMenuDialogConfig;
                     
                 carouselButton = new Button({
                     model: {
                         text: 'Display Carousel',
                     },
-                    click: this.carouselButtonClick
+                    click: this.carouselButtonClick.bind(this)
                 });
 
                 contextMenuButton = new Button({
                     model: {
                         text: 'Display Context Menu',
                     },
-                    click: this.contextMenuButtonClick
+                    click: this.contextMenuButtonClick.bind(this)
                 });
+
+                carouselDialogConfig = new DialogConfig.ListDialog({}).build();
+                this.carouselDialog = new Dialog(carouselDialogConfig);
+
+                contextMenuDialogConfig = new DialogConfig.ListDialog({}).build();
+                this.contextMenuDialog = new Dialog(contextMenuDialogConfig);
 
                 carouselButton.render(this.getView());
                 contextMenuButton.render(this.getView());
@@ -30,11 +38,11 @@ define(function (require) {
         }
 
         carouselButtonClick () {
-            
+            this.carouselDialog.show();
         }
 
         contextMenuButtonClick () {
-            
+            this.contextMenuDialog.show();
         }
     };
 });
