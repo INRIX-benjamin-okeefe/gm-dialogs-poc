faaf78657e229a853a5cf847d56b33b77746dbb5
Benjamin O'Keefe <benjamin.okeefe@inrix.com>
2017-04-03 10:51:47 -0700

    Finished images view

--- screens/view-images-chamber.js
+++ screens/view-images-chamber.js
@@ -4,6 +4,22 @@ define(function (require) {
 	const ListChamber = require('common/platform/chamber/ListChamber');
 
 	return class extends ListChamber {
-		
+		getListConfig () {
+			return {
+				layout: 'hero',
+				orientation: 'horizontal',
+				itemClick: (eventKey, listControl, model, details) => {
+					console.log(model);
+				}
+			}
+		}
+
+		data () {
+			return [
+				{ icon: `${window.MODULE_PATH}/images/yelp1.jpg` },
+				{ icon: `${window.MODULE_PATH}/images/yelp2.jpg` },
+				{ icon: `${window.MODULE_PATH}/images/yelp3.jpg` }
+			];
+		}
 	}
 });
